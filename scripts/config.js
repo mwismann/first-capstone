943f5b399cf2879dacd71efedc38afc4ad3b80ae
Matt <mwbertolucci@gmail.com>
2023-02-07 19:10:57 -0300

    Implement user interactions

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,22 @@
-const body = document.querySelector('body');
-body.style.color = '#fff';
+// ---------- Variables ---------------
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const openMobileBtn = document.querySelector('.open-mobile');
+const closeMobileBtn = document.querySelectorAll('.close-mobile');
+
+// --------- Functions ----------------
+
+const openMobileMenu = () => {
+    mobileMenu.classList.remove('hidden');
+    openMobileBtn.classList.add('hidden');
+} 
+
+const closeMobileMenu = () => {
+    mobileMenu.classList.add('hidden');
+    openMobileBtn.classList.remove('hidden');
+}
+
+// --------- eventListeners -----------
+
+openMobileBtn.addEventListener('click', openMobileMenu);
+closeMobileBtn.forEach(btn => btn.addEventListener('click', closeMobileMenu));
