f69c136e62b4a22a9c59825b399a2de81481679f
Matt <mwbertolucci@gmail.com>
2023-02-08 15:40:31 -0300

    Store speakers data in an object for dynamic content

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,43 @@
+// --------- Objects Data ------------------------------
+const speakersData = [
+  {
+    name: 'Gary Marcus',
+    img: 'img/gary-marcus.jpg',
+    highlight: 'Professor emeritus of psychology and neural science at New York University and author',
+    description: 'In 2014 he founded Geometric Intelligence, a machine-learning company. His books include Guitar Zero and Kluge',
+  },
+  {
+    name: 'Demis Hassabis',
+    img: 'img/demis-hassabis.jpg',
+    highlight: 'Artificial Intelligence researcher, entrepreneur, and UK Government AI Advisor',
+    description: 'Chief executive and co-founder of DeepMind and Insomorphic Labs',
+  },
+  {
+    name: 'Fei-Fei Li',
+    img: 'img/fei-fei-li.jpg',
+    highlight: 'Sequoia Capital Professor of Computer Science at Standford University',
+    description: 'Established ImageNet, the dataset that enabled rapid advances in computer vision in the 2010s',
+  },
+  {
+    name: 'Cynthia Breazeal',
+    img: 'img/cynthia-breazeal.jpg',
+    highlight: 'Professor of media arts and science at MIT',
+    description: 'Founded and directs the Personal Robots group at MIT\'s Media Lab',
+  },
+  {
+    name: 'Raymond Kurzweil',
+    img: 'img/raymond-kurzweil.jpg',
+    highlight: 'Computer scientist, author, investor, and futurist',
+    description: 'Involved in fields such as optical character recognition, text-to-speech synthesis, and speech recognition technology',
+  },
+  {
+    name: 'Jurgen Schmindhuber',
+    img: 'img/jurgen-schmidhuber.jpg',
+    highlight: 'Renown in the fields of artificial intelligence, deep learning and artificial neural networks',
+    description: 'Co-director of the Dalle Molle Institute for Artificial Intelligence Research in Lugano, Switzerland',
+  },
+];
+
 // --------- Header Variables ---------------------------
 const mobileMenu = document.querySelector('.mobile-menu');
 const openMobileBtn = document.querySelector('.open-mobile');
@@ -28,78 +68,24 @@ const renderSpeakersSection = () => {
     </div>
 
     <div class="speakers-grid pl-2 pr-3 grid grid-cols-1 grid-rows-6 gap-8 md:grid-cols-2 md:grid-rows-3 md:gap-x-2 md:px-12 lg:px-36 xl:mt-8 xl:px-60 xl:gap-y-24">
-        <div class="speaker-card relative flex gap-16 md:gap-20 lg:gap-24 xl:gap-20">
-            <i class="las la-chess-board text-7xl sm:text-8xl md:text-7xl md:text-[#d3d3d3] xl:text-8xl"></i>
-            <div class="card-img-container absolute top-6 left-6 w-1/4 h-4/5 bg-[url('img/gary-marcus.jpg')] bg-cover max-[320px]:w-1/3 sm:w-1/6 sm:h-full md:w-2/6 md:h-2/3 lg:h-4/5 xl:w-1/4 xl:h-36"></div>
-            <span class="sr-only">A picture of Gary Marcus</span>
-            <div class="card-meta flex flex-col justify-center items-start text-start">
-                <h3 class="font-semibold xl:text-2xl">Gary Marcus</h3>
-                <p class="highlight text-xs italic xl:text-base">Professor emeritus of psychology and neural science at New York University
-                    and author</p>
-                    <i class="las la-minus"></i>
-                <p class="text-xs xl:text-base">In 2014 he founded Geometric Intelligence, a machine-learning company. His books include Guitar Zero and Kluge</p>
-            </div>
-        </div>
-        <div class="speaker-card relative flex gap-16 md:gap-20 lg:gap-24 xl:gap-20">
-            <i class="las la-chess-board text-7xl sm:text-8xl md:text-7xl md:text-[#d3d3d3] xl:text-8xl"></i>
-            <div class="card-img-container absolute top-6 left-6 w-1/4 h-4/5 bg-[url('img/demis-hassabis.jpg')] bg-cover max-[320px]:w-1/3 sm:w-1/6 sm:h-full md:w-2/6 md:h-2/3 lg:h-4/5 xl:w-1/4 xl:h-36"></div>
-            <span class="sr-only">A picture of Demis Hassabis</span>
-            <div class="card-meta flex flex-col justify-center items-start text-start">
-                <h3 class="font-semibold">Demis Hassabis</h3>
-                <p class="highlight text-xs italic xl:text-base">Artificial Intelligence researcher, entrepreneur, and UK Government AI Advisor</p>
-                <i class="las la-minus"></i>
-                <p class="text-xs xl:text-base">Chief executive and co-founder of DeepMind and Insomorphic Labs</p>
-            </div>
-        </div>
-        <div class="speaker-cards relative flex gap-16 md:gap-20 lg:gap-24 xl:gap-20">
-            <i class="las la-chess-board text-7xl sm:text-8xl md:text-7xl md:text-[#d3d3d3] xl:text-8xl"></i>
-            <div class="card-img-container absolute top-6 left-6 w-1/4 h-4/5 bg-[url('img/fei-fei-li.jpg')] bg-cover max-[320px]:w-1/3 sm:w-1/6 sm:h-full md:w-2/6 md:h-2/3 lg:h-4/5 xl:w-1/4 xl:h-36"></div>
-            <span class="sr-only">A picture of Fei-Fei Li</span>
-            <div class="card-meta flex flex-col justify-center items-start text-start">
-                <h3 class="font-semibold">Fei-Fei Li</h3>
-                <p class="highlight text-xs italic xl:text-base">Sequoia Capital Professor of Computer Science at Standford University</p>
-                <i class="las la-minus"></i>
-                <p class="text-xs xl:text-base">Established ImageNet, the dataset that enabled rapid advances in computer vision in the 2010s</p>
-            </div>
-        </div>
-        <div class="speaker-card relative flex gap-16 md:gap-20 lg:gap-24 xl:gap-20">
-            <i class="las la-chess-board text-7xl sm:text-8xl md:text-7xl md:text-[#d3d3d3] xl:text-8xl"></i>
-            <div class="card-img-container absolute top-6 left-6 w-1/4 h-4/5 bg-[url('img/cynthia-breazeal.jpg')] bg-cover max-[320px]:w-1/3 sm:w-1/6 sm:h-full md:w-2/6 md:h-2/3 lg:h-4/5 xl:w-1/4 xl:h-36"></div>
-            <span class="sr-only">A picture of Cynthia Breazeal</span>
-            <div class="card-meta flex flex-col justify-center items-start text-start">
-                <h3 class="font-semibold">Cynthia Breazeal</h3>
-                <p class="highlight text-xs italic xl:text-base">Professor of media arts and science at MIT</p>
-                <i class="las la-minus"></i>
-                <p class="text-xs xl:text-base">Founded and directs the Personal Robots group at MIT's Media Lab</p>
-            </div>
-        </div>
-        <div class="speaker-card relative flex gap-16 md:gap-20 lg:gap-24 xl:gap-20">
-            <i class="las la-chess-board text-7xl sm:text-8xl md:text-7xl md:text-[#d3d3d3] xl:text-8xl"></i>
-            <div class="card-img-container absolute top-6 left-6 w-1/4 h-4/5 bg-[url('img/raymond-kurzweil.jpg')] bg-cover max-[320px]:w-1/3 sm:w-1/6 sm:h-full md:w-2/6 md:h-2/3 lg:h-4/5 xl:w-1/4 xl:h-36"></div>
-            <span class="sr-only">A picture of Raymond Kurzweil</span>
-            <div class="card-meta flex flex-col justify-center items-start text-start">
-                <h3 class="font-semibold">Raymond Kurzweil</h3>
-                <p class="highlight text-xs italic xl:text-base">Computer scientist, author, investor, and futurist</p>
-                <i class="las la-minus"></i>
-                <p class="text-xs xl:text-base">Involved in fields such as optical character recognition,
-                    text-to-speech synthesis, and speech recognition technology</p>
-            </div>
-        </div>
-        <div class="speaker-card relative flex gap-16 md:gap-20 lg:gap-24 xl:gap-20">
-            <i class="las la-chess-board text-7xl sm:text-8xl md:text-7xl md:text-[#d3d3d3] xl:text-8xl"></i>
-            <div class="card-img-container absolute top-6 left-6 w-1/4 h-4/5 bg-[url('img/jurgen-schmidhuber.jpg')] bg-cover max-[320px]:w-1/3 sm:w-1/6 sm:h-full md:w-2/6 md:h-2/3 lg:h-4/5 xl:w-1/4 xl:h-36"></div>
-            <span class="sr-only">A picture of Jurgen Schmindhuber</span>
-            <div class="card-meta flex flex-col justify-center items-start text-start">
-                <h3 class="font-semibold">Jurgen Schmindhuber</h3>
-                <p class="highlight text-xs italic xl:text-base">Renown in the fields of artificial intelligence,
-                    deep learning and artificial neural networks</p>
-                    <i class="las la-minus"></i>
-                <p class="text-xs xl:text-base">Co-director of the Dalle Molle Institute for Artificial Intelligence Research in Lugano, Switzerland</p>
-            </div>
-        </div>
     </div>`;
 
   speakersSection.insertAdjacentHTML('beforeend', speakersContent);
+
+  const speakersGrid = document.querySelector('.speakers-grid');
+
+  speakersData.forEach((speaker) => speakersGrid.insertAdjacentHTML('beforeend',
+    `<div class="speaker-card relative flex gap-16 md:gap-20 lg:gap-24 xl:gap-20">
+    <i class="las la-chess-board text-7xl sm:text-8xl md:text-7xl md:text-[#d3d3d3] xl:text-8xl"></i>
+    <div class="card-img-container absolute top-6 left-6 w-1/4 h-4/5 bg-[url(${speaker.img})] bg-cover max-[320px]:w-1/3 sm:w-1/6 sm:h-full md:w-2/6 md:h-2/3 lg:h-4/5 xl:w-1/4 xl:h-36"></div>
+    <span class="sr-only">A picture of ${speaker.name}</span>
+    <div class="card-meta flex flex-col justify-center items-start text-start">
+        <h3 class="font-semibold">${speaker.name}</h3>
+        <p class="highlight text-xs italic xl:text-base">${speaker.highlight}</p>
+        <i class="las la-minus"></i>
+        <p class="text-xs xl:text-base">${speaker.description}</p>
+    </div>
+    </div>`));
 };
 
 // --------- eventListeners -----------------------------
